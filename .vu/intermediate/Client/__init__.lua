783bba2bc838dcd6982f3b213bd8cfd94fb4acc0
IllustrisJack <76592751+IllustrisJack@users.noreply.github.com>
2022-08-20 15:47:06 +0200

    Delete .vu/intermediate directory

--- .vu/intermediate/Client/__init__.lua
+++
@@ -1,9 +0,0 @@
---[[
-    Venice Unleashed - Intermediate Lua binding
-    Type: __init__.lua
-    Generated on: Fri Aug 19 2022 20:30:23 GMT+0200 (Mitteleuropäische Sommerzeit)
---]]
-
----@meta
-
-
